8b3fed61bae03e9a15de73eb4b77a41b41a42e04
Daniel Grad <daniel.g@kaholo.io>
2022-04-14 16:54:19 +0200

    Created the plugin and added listRecords method

--- app.js
+++ app.js
@@ -1,8 +1,86 @@
-function hello(name) {
-  const greeting = `hello ${name}`;
-  return greeting;
+const fetch = require("node-fetch");
+const { bootstrap } = require("kaholo-plugin-library");
+
+const API_URL = "https://api.airtable.com/v0";
+
+async function listRecords(params) {
+  const {
+    apiKey,
+    base,
+    table,
+    view,
+    fieldsFilter,
+    formulaFilter,
+    sortBy,
+    sortOrder,
+    totalMaxRecords,
+  } = params;
+
+  const listRecordsUrl = new URL(`${API_URL}/${base}/${table}`);
+  const requestOptions = { headers: { Authorization: `Bearer ${apiKey}` } };
+
+  // this mutates the first argument
+  setUrlParams(listRecordsUrl.searchParams, {
+    view,
+    fieldsFilter,
+    formulaFilter,
+    sortBy,
+    sortOrder,
+    totalMaxRecords,
+  });
+
+  let response = await fetch(listRecordsUrl.toString(), requestOptions);
+  let result = await response.json();
+  let records = [...result.records];
+  let { offset } = result;
+
+  /* eslint-disable no-await-in-loop */
+  while (offset) {
+    listRecordsUrl.searchParams.set("offset", offset);
+    response = await fetch(listRecordsUrl.toString(), requestOptions);
+    result = await response.json();
+    offset = result.offset;
+    records = records.concat(result.records);
+  }
+  /* eslint-enable no-await-in-loop */
+
+  return records;
 }
 
-module.exports = {
-  hello,
-};
+function setUrlParams(urlParams, {
+  fieldsFilter,
+  formulaFilter,
+  view,
+  sortBy,
+  sortOrder,
+  totalMaxRecords,
+}) {
+  if (fieldsFilter) {
+    fieldsFilter.forEach((filter) => urlParams.append("fields", filter));
+  }
+
+  if (formulaFilter) {
+    urlParams.set("filterByFormula", formulaFilter);
+  }
+
+  if (view) {
+    urlParams.set("view", view);
+  }
+
+  if (sortBy || sortOrder) {
+    if (!sortBy || !sortOrder) {
+      throw new Error("Sorting requires both \"Sort by\" and \"Sort order\" parameters");
+    }
+
+    urlParams.set("sort[0][field]", sortBy);
+    urlParams.set("sort[0][direction]", sortOrder);
+  }
+
+  if (totalMaxRecords) {
+    urlParams.set("maxRecords", totalMaxRecords);
+  }
+}
+
+module.exports = bootstrap({
+  listRecords,
+}, {});
